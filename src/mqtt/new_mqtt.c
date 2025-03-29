947ff6775b12ed1488ae41f04377d7edf8d6ae3c
Regin Larsen <kvakulo@gmail.com>
2025-03-29 15:08:06 +0100

    logging

--- src/mqtt/new_mqtt.c
+++ src/mqtt/new_mqtt.c
@@ -1036,6 +1036,7 @@ int MQTT_process_received(){
 // called from tcp_thread context
 static void mqtt_incoming_publish_cb(void* arg, const char* topic, u32_t tot_len)
 {
+	addLogAdv(LOG_INFO, LOG_FEATURE_MQTT, "mqtt_incoming_publish_cb");
 	//const char *p;
 	int i;
 	// unused - left here as example
@@ -1045,9 +1046,11 @@ static void mqtt_incoming_publish_cb(void* arg, const char* topic, u32_t tot_len
 	g_mqtt_request.topic[0] = '\0';
 	for (i = 0; i < numCallbacks; i++)
 	{
+	    addLogAdv(LOG_INFO, LOG_FEATURE_MQTT, "mqtt_incoming_publish_cb callback %i", i);	
 		char* cbtopic = callbacks[i]->topic;
 		if (strncmp(topic, cbtopic, strlen(cbtopic)))
 		{
+			addLogAdv(LOG_INFO, LOG_FEATURE_MQTT, "mqtt_incoming_publish_cb strncmp");
 			strncpy(g_mqtt_request.topic, topic, sizeof(g_mqtt_request.topic) - 1);
 			g_mqtt_request.topic[sizeof(g_mqtt_request.topic) - 1] = 0;
 			break;
